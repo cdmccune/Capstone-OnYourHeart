915b5f1bacfce13ee77fd7f42d62870a30139db9
cdmccune <curtmccune@gmail.com>
2022-06-29 16:16:11 -0700

    started working on auto-text color, add list functionality almost done

--- OnYourHeart-Pod/Controllers/ViewControllers/Lists/AddListViewController.swift
+++ OnYourHeart-Pod/Controllers/ViewControllers/Lists/AddListViewController.swift
@@ -9,13 +9,84 @@ import UIKit
 
 class AddListViewController: UIViewController {
 
+    //MARK: - Properties
+    var color: UIColor?
+    var colorWell: UIColorWell!
+    var isMood = false
+    @IBOutlet var listNameTextField: UITextField!
+    @IBOutlet var colorWellView: UIView!
+    @IBOutlet var moodButton: UIButton!
+    @IBOutlet var createListButton: UIButton!
+    
+    //MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
+    
+    //MARK: - Helper Functions
+    func updateViews() {
+        
+        colorWell = UIColorWell(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        colorWellView.addSubview(colorWell)
+        colorWell.addTarget(self, action: #selector(colorWellValueChanged), for: .valueChanged)
+        colorWell.title = "Choose a Color"
+        colorWell.supportsAlpha = false
+        
+        var actions: [UIAction] = []
+        
+        let noAction = UIAction(title: "No") { _ in
+            self.isMood = false
+            self.moodButton.setTitle("No", for: .normal)
+        }
+        
+        let yesAction = UIAction(title: "Yes") { _ in
+            self.isMood = true
+            self.moodButton.setTitle("Yes", for: .normal)
+        }
+        
+        actions.append(noAction)
+        actions.append(yesAction)
+        let menu = UIMenu(title: "Is it a mood?", options: .displayInline, children: actions)
+        moodButton.menu = menu
+        moodButton.showsMenuAsPrimaryAction = true
+        
+        
+        
+        
+    }
+    
+    @objc func colorWellValueChanged(_ sender: Any) {
+        self.color = colorWell.selectedColor
+        
+        if self.color != nil, let text = listNameTextField.text, text != "" {
+            createListButton.isEnabled = true
+        }
+    }
+    
+    
+    @IBAction func textFieldChanged(_ sender: Any) {
+        
+        if self.color != nil, let text = listNameTextField.text, text != "" {
+            createListButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func createListButtonTapped(_ sender: Any) {
+        guard let text = listNameTextField.text, text != "", let color = color else {return}
+        
+        let rgb = ColorUtilities.getRGBFromColor(color: color)
+        let textColor = ColorUtilities.blackOrWhiteText(color: color).rawValue
+        
+        print(textColor)
+        
+        let newList = ListItem(name: text, color:rgb, textColor: textColor)
+        
+        //FireBase add new list
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -27,3 +98,5 @@ class AddListViewController: UIViewController {
     */
 
 }
+
+
--- OnYourHeart-Pod/Helpers/Colors.swift
+++ OnYourHeart-Pod/Helpers/Colors.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 import AVFoundation
+import CoreGraphics
 
 struct Colors {
     static var backgroundTan = UIColor(red: 242/255, green: 239/255, blue: 227/255, alpha: 1.0)
@@ -18,6 +19,23 @@ struct Colors {
 class ColorUtilities {
     static func getColorsFromRGB(rGB: [Double]) -> UIColor {
         return UIColor(red: rGB[0]/255, green: rGB[1]/255, blue: rGB[2]/255, alpha: 1.0)
+    }
+    static func getRGBFromColor(color: UIColor) -> [Double] {
+        var colors: [Double] = []
+        colors.append(CIColor(color: color).red)
+        colors.append(CIColor(color: color).green)
+        colors.append(CIColor(color: color).blue)
+        
+        return colors
+    }
+    static func blackOrWhiteText(color: UIColor) -> MoodColor {
+        let colorComponents = (color.cgColor).components
+        
+        guard let components = colorComponents, components.count >= 3 else {return .black}
+        
+        let brightness = Float(((components[0] * 299) + (components[1] * 587) + (components[2] * 114)) / 1000)
+        
+        return brightness > 0.7 ? .white : .black
     }
 }
 
