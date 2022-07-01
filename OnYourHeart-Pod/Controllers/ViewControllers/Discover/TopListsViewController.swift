554651017eef8d80fd49717778e75dde355ce848
cdmccune <curtmccune@gmail.com>
2022-06-30 17:33:23 -0700

    started on displaying top list

--- OnYourHeart-Pod/Controllers/ViewControllers/Discover/TopListsViewController.swift
+++ OnYourHeart-Pod/Controllers/ViewControllers/Discover/TopListsViewController.swift
@@ -35,64 +35,9 @@ class TopListsViewController: UIViewController {
 }
 
 extension TopListsViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 60.0
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-       
-        let list = FirebaseDataController.shared.lists[section]
-        let backgroundColor = ColorUtilities.getColorsFromRGB(rGB: list.color)
-        
-        
-        let myLabel = UILabel()
-        myLabel.frame = CGRect(x: 20, y:0, width: 320, height: 60)
-        myLabel.font = UIFont.boldSystemFont(ofSize: 25)
-        myLabel.text = self.tableView(tableView, titleForHeaderInSection: section)
-        myLabel.textColor = MoodColor(rawValue: list.textColor)?.create ?? .black
-        
-        let myButton = UIButton()
-        myButton.frame = CGRect(x: view.frame.width - 120, y: 0, width: 100, height: 60)
-        myButton.setTitle("See All", for: .normal)
-        myButton.setTitleColor(MoodColor(rawValue: list.textColor)?.create ?? .black, for: .normal)
-        
-        myButton.tag = section
-        
-        myButton.addTarget(self, action: #selector(seeAllButtonTapped), for: .touchUpInside)
-        
-//        myButton.backgroundColor = .blue
-        
-    
-        let headerView = UIView()
-        
-        headerView.backgroundColor = backgroundColor
-        headerView.addSubview(myLabel)
-        headerView.addSubview(myButton)
-        
-        return headerView
-        
-    }
-    
-    @objc func seeAllButtonTapped(sender: UIButton) {
-        
-        FirebaseDataController.shared.currentListTag = sender.tag
-        guard let listDetailVC = self.storyboard?.instantiateViewController(withIdentifier: Constants.Storyboard.listDetailVC) as? UIViewController else {return}
-        
-        self.navigationController?.pushViewController(listDetailVC, animated: true)
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return FirebaseDataController.shared.lists.count
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return FirebaseDataController.shared.lists[section].name
-        
-    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let count = FirebaseDataController.shared.lists[section].scriptureListEntries.count
-        
-        return count < 3 ? count : 3
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
